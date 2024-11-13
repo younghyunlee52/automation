c0f1bde03025ef4bbca0beb57cedbcaf47339db7
heewon-yoon <hy2571@columbia.edu>
2024-11-13 17:27:41 -0500

    11/13/2024

--- pilotdata.R
+++ pilotdata.R
@@ -2,5 +2,3 @@ library(tidyverse)
 
 back <- read.csv("/Users/hyoon/Desktop/Yoon2/automation/experiment/automation/Subject Pool - Fall 2024 - Background Survey_November 4, 2024_16.16.csv")
 exp <- read.csv("/Users/hyoon/Desktop/Yoon2/automation/experiment/automation/Subject Pool - Fall 2024 - wave 1 - Lee_November 4, 2024_16.22.csv")
-
-
